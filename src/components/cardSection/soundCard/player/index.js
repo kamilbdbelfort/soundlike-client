52609955b01ac82741d6d4796366bd4344484515
kamilbdbelfort <k.beelidebelfort@hotmail.com>
2021-10-21 17:16:15 +0200

    created a player component, working

---
+++ src/components/cardSection/soundCard/player/index.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
+
+const useAudio = (url) => {
+  const [audio] = useState(new Audio(url));
+  const [playing, setPlaying] = useState(false);
+
+  const toggle = () => setPlaying(!playing);
+
+  useEffect(() => {
+    playing ? audio.play() : audio.pause();
+  }, [playing]);
+
+  useEffect(() => {
+    audio.addEventListener("ended", () => setPlaying(false));
+    return () => {
+      audio.removeEventListener("ended", () => setPlaying(false));
+    };
+  }, []);
+
+  return [playing, toggle];
+};
+
+const Player = ({ url }) => {
+  const [playing, toggle] = useAudio(url);
+
+  return (
+    <div>
+      <Button
+        variant="success"
+        style={{ width: "100%", fontSize: "36px" }}
+        onClick={toggle}
+      >
+        {playing ? "⏸" : "▶️"}
+      </Button>
+    </div>
+  );
+};
+
+export default Player;
